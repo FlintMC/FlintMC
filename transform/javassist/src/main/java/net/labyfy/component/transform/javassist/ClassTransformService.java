25bd7dfe6b4a3e1cb3fbeb515989b8d65127b9db
Jan Brachthäuser <contact@jan-br.de>
2020-03-14 21:28:12 +0100

    Add ClassTransform

---
+++ transform/javassist/src/main/java/net/labyfy/component/transform/javassist/ClassTransformService.java
@@ -0,0 +1,13 @@
+package net.labyfy.component.transform.javassist;
+
+import net.labyfy.base.structure.identifier.Identifier;
+import net.labyfy.base.structure.service.Service;
+import net.labyfy.base.structure.service.ServiceHandler;
+
+@Service(ClassTransform.class)
+public class ClassTransformService implements ServiceHandler {
+
+  public void discover(Identifier.Base property) {
+
+  }
+}
