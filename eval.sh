fd4e1f455ec868ad61d97b3eb338e25a332c9eeb
Marcus <xiantiel@hotmail.de>
2020-03-23 11:24:26 +0100

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat build.gradle
-sed "s|\${artifactory_contextUrl}'|${artifactory_contextUrl}|g" build.gradle
+sed "s|\${artifactory_contextUrl}|${artifactory_contextUrl}|g" build.gradle
 cat build.gradle
