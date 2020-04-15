ad1adfc9c02142a6183b911a4e51b69648914139
xiantiel <xiantiel@hotmail.de>
2020-04-15 11:55:59 +0200

    eval

--- eval.sh
+++ eval.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #insert ci variables in files before gradle build process
 sed -i "s|\${artifactory_contextUrl}|${artifactory_contextUrl}|g" build.gradle
-cat build.gradle
+sed -i "s|\${artifactory_user}|${artifactory_user}|g" build.gradle
+sed -i "s|\${artifactory_password}|${artifactory_password}|g" build.gradle
+
+sed -i "s|\${artifactory_contextUrl}|${artifactory_contextUrl}|g" settings.gradle
+sed -i "s|\${artifactory_user}|${artifactory_user}|g" settings.gradle
+sed -i "s|\${artifactory_password}|${artifactory_password}|g" settings.gradle
