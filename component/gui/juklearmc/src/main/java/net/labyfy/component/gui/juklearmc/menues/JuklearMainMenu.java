a61d496659ba98b3ee50fb23c182d7a1e0b0e245
Janrupf <werbung.janrupf@t-online.de>
2020-06-06 23:49:58 +0200

    Use real window bounding instead of screen bounding

--- component/gui/juklearmc/src/main/java/net/labyfy/component/gui/juklearmc/menues/JuklearMainMenu.java
+++ component/gui/juklearmc/src/main/java/net/labyfy/component/gui/juklearmc/menues/JuklearMainMenu.java
@@ -6,34 +6,34 @@ import net.janrupf.juklear.layout.component.JuklearWindow;
 import net.labyfy.component.gui.Gui;
 import net.labyfy.component.gui.GuiRenderState;
 import net.labyfy.component.gui.Guis;
+import net.labyfy.component.gui.MinecraftWindow;
 import net.labyfy.component.gui.juklearmc.JuklearMC;
-import net.labyfy.component.gui.juklearmc.wrapper.ScreenSizeProvider;
 import net.labyfy.component.transform.hook.Hook;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Singleton
 @Gui(Guis.GUI_MAIN_MENU)
 public class JuklearMainMenu {
+  private final MinecraftWindow minecraftWindow;
   private final JuklearMC juklearMC;
   private final JuklearWindow window;
 
   @Inject
-  private JuklearMainMenu(JuklearMC juklearMC) {
+  private JuklearMainMenu(MinecraftWindow minecraftWindow, JuklearMC juklearMC) {
+    this.minecraftWindow = minecraftWindow;
     this.juklearMC = juklearMC;
     JuklearContext context = juklearMC.getContext();
 
-    this.window = new JuklearWindow("Hello, here is Juklear in Minecraft!");
+    this.window = new JuklearWindow("Hello, here is Juklear in Minecraft!", 0, 0, 600, 400);
     this.window.addFlag(JuklearPanelFlags.TITLE);
     this.window.addFlag(JuklearPanelFlags.BORDER);
     context.addTopLevel(window);
   }
 
   @GuiRenderState(value = GuiRenderState.Type.RENDER, executionTime = Hook.ExecutionTime.AFTER)
-  public void render(@Named("instance") Object instance) {
-    ScreenSizeProvider screenSizeProvider = (ScreenSizeProvider) instance;
-    juklearMC.draw(screenSizeProvider.getWidth(), screenSizeProvider.getHeight());
+  public void render() {
+    juklearMC.draw((int) minecraftWindow.getWidth(), (int) minecraftWindow.getHeight());
   }
 }
--- component/gui/juklearmc/src/main/java/net/labyfy/component/gui/juklearmc/wrapper/ScreenSizeProvider.java
+++
@@ -1,6 +0,0 @@
-package net.labyfy.component.gui.juklearmc.wrapper;
-
-public interface ScreenSizeProvider {
-  int getWidth();
-  int getHeight();
-}
