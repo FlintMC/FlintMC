7ada441851c606011b08fd58dcb592a1a8a92676
Jan <contact@jan-br.de>
2020-06-24 11:36:59 +0200

    - add comment

--- component/stereotype/src/main/java/net/labyfy/component/stereotype/annotation/Transitive.java
+++ component/stereotype/src/main/java/net/labyfy/component/stereotype/annotation/Transitive.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Mark annotations that should be transitively resolvable.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface Transitive {}
