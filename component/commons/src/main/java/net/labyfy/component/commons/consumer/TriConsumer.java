f944a493dd714f92822d6e1c141c2f8eb1d3a76d
janrupf <business.janrupf@gmail.com>
2020-06-25 15:18:32 +0200

    Refactor and document commons module

--- component/commons/src/main/java/net/labyfy/component/commons/consumer/TriConsumer.java
+++ component/commons/src/main/java/net/labyfy/component/commons/consumer/TriConsumer.java
@@ -1,13 +1,20 @@
 package net.labyfy.component.commons.consumer;
 
+/**
+ * Utility class for a consumer accept 3 values.
+ *
+ * @param <A> The type of the first argument
+ * @param <B> The type of the second argument
+ * @param <C> The type of the third argument
+ */
 @FunctionalInterface
-public interface TriConsumer<K, V, S> {
-
+public interface TriConsumer<A, B, C> {
   /**
    * Performs the operation given the specified arguments.
-   * @param k the first input argument
-   * @param v the second input argument
-   * @param s the third input argument
+   *
+   * @param a the first input argument
+   * @param b the second input argument
+   * @param c the third input argument
    */
-  void accept(K k, V v, S s);
+  void accept(A a, B b, C c);
 }
