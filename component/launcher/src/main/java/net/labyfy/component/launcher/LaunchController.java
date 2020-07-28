85efba6af3500da474a948eae35ce9b36764a57c
PublicByte <publicbyte+labymedia+gitlab@s7stud.io>
2020-07-28 12:58:05 +0200

    Update mapping format

--- component/launcher/src/main/java/net/labyfy/component/launcher/LaunchController.java
+++ component/launcher/src/main/java/net/labyfy/component/launcher/LaunchController.java
@@ -52,8 +52,8 @@ public class LaunchController {
 
     /**
      * Executes the launch. This is effectively the new `main` method.
-     *
-     * @implNote Called by the {@link LabyLauncher} using reflection
+     * <p>
+     * <b>Called by the {@link LabyLauncher} using reflection</b>
      */
     public void run() {
         logger.info("Initializing LaunchController");
--- component/mapping/src/main/java/net/labyfy/component/mappings/McpMappingIndexProvider.java
+++ component/mapping/src/main/java/net/labyfy/component/mappings/McpMappingIndexProvider.java
@@ -1,23 +1,28 @@
 package net.labyfy.component.mappings;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.Singleton;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
 @Singleton
 public class McpMappingIndexProvider {
-
-  private final static String INDEX_URL = "https://dl.labymod.net/mappings/index.json";
+  private final static String INDEX_URL = "https://dl.labymod.net/mappings/index_new.json";
   private final static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
-  public McpMappingIndex fetch() throws IOException {
-    try (InputStreamReader reader = new InputStreamReader(new URL(INDEX_URL).openStream())) {
-      return gson.fromJson(reader, McpMappingIndex.class);
+  public Map<String, Version> fetch() throws IOException {
+    HttpURLConnection connection = (HttpURLConnection) new URL(INDEX_URL).openConnection();
+    connection.setRequestProperty("User-Agent", "Labyfy/0.2.1 (+https://www.labymod.net/)");
+    connection.connect();
+
+    try (InputStreamReader reader = new InputStreamReader(connection.getInputStream())) {
+      return gson.fromJson(reader, new TypeToken<Map<String, Version>>() {}.getType());
     }
   }
-
 }
