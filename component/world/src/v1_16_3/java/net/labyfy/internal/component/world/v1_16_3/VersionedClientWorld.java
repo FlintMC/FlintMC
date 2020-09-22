fc55cf627f5d58484973f4d6494c280773445949
R0bbyYT <r0bby@gmx.net>
2020-09-22 14:08:08 +0200

    1.16.3 implementation

---
+++ component/world/src/v1_16_3/java/net/labyfy/internal/component/world/v1_16_3/VersionedClientWorld.java
@@ -0,0 +1,92 @@
+package net.labyfy.internal.component.world.v1_16_3;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import net.labyfy.component.inject.implement.Implement;
+import net.labyfy.component.player.Player;
+import net.labyfy.component.world.ClientWorld;
+import net.labyfy.component.world.border.WorldBorder;
+import net.labyfy.component.world.difficult.DifficultyLocal;
+import net.labyfy.component.world.scoreboad.Scoreboard;
+import net.labyfy.component.world.util.BlockPosition;
+import net.minecraft.client.Minecraft;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * 1.15.2 implementation of {@link ClientWorld}
+ */
+@Singleton
+@Implement(value = ClientWorld.class, version = "1.15.2")
+public class VersionedClientWorld extends VersionedWorld implements ClientWorld {
+
+  private final List<Player> players;
+  private final Scoreboard scoreboard;
+
+  @Inject
+  public VersionedClientWorld(
+          BlockPosition.Factory blockPositionFactory,
+          DifficultyLocal.Factory difficultyLocalFactory,
+          Scoreboard scoreboard,
+          WorldBorder worldBorder
+  ) {
+    super(blockPositionFactory, difficultyLocalFactory, worldBorder);
+    this.scoreboard = scoreboard;
+    this.players = new ArrayList<>();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int getEntityCount() {
+    return Minecraft.getInstance().world.getCountLoadedEntities();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean addPlayer(Player player) {
+    return this.players.add(player);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean removePlayer(UUID uniqueId) {
+    return this.players.removeIf(predicatePlayer ->
+            predicatePlayer
+                    .getGameProfile()
+                    .getUniqueId()
+                    .equals(uniqueId)
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<Player> getPlayers() {
+    return this.players;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int getPlayerCount() {
+    return this.players.size();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Scoreboard getScoreboard() {
+    return this.scoreboard;
+  }
+}
---
+++ component/world/src/v1_16_3/java/net/labyfy/internal/component/world/v1_16_3/scoreboard/score/VersionedPlayerTeam.java
@@ -0,0 +1,228 @@
+package net.labyfy.internal.component.world.v1_16_3.scoreboard.score;
+
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import net.labyfy.chat.component.ChatComponent;
+import net.labyfy.chat.format.ChatColor;
+import net.labyfy.component.inject.implement.Implement;
+import net.labyfy.component.world.scoreboad.Scoreboard;
+import net.labyfy.component.world.scoreboad.score.PlayerTeam;
+import net.labyfy.component.world.scoreboad.type.CollisionType;
+import net.labyfy.component.world.scoreboad.type.VisibleType;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Robby
+ */
+@Implement(value = PlayerTeam.class, version = "1.15.2")
+public class VersionedPlayerTeam implements PlayerTeam {
+
+  private final Set<String> members = new HashSet<>();
+
+  private final Scoreboard scoreboard;
+  private final String name;
+
+  private boolean allowFriendlyFire;
+  private boolean canSeeFriendlyInvisible;
+
+  private ChatComponent displayName;
+  private ChatComponent prefix;
+  private ChatComponent suffix;
+
+  private VisibleType nameTagVisibility;
+  private VisibleType deathMessageVisibility;
+  private ChatColor color;
+  private CollisionType collisionType;
+
+  @AssistedInject
+  private VersionedPlayerTeam(
+          @Assisted("scoreboard") Scoreboard scoreboard,
+          @Assisted("name") String name,
+          @Assisted("chatComponent") ChatComponent displayName) {
+    this.scoreboard = scoreboard;
+    this.name = name;
+    this.displayName = displayName;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ChatComponent getDisplayName() {
+    return this.displayName;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setDisplayName(ChatComponent displayName) {
+    this.displayName = displayName;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ChatComponent getPrefix() {
+    return this.prefix;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setPrefix(ChatComponent prefix) {
+    this.prefix = prefix;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ChatComponent getSuffix() {
+    return this.suffix;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setSuffix(ChatComponent suffix) {
+    this.suffix = suffix;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int getFriendlyFlags() {
+    int flag = 0;
+
+    if (this.allowFriendlyFire()) {
+      flag |= 1;
+    }
+
+    if (this.seeFriendlyInvisible()) {
+      flag |= 2;
+    }
+
+    return flag;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setColor(ChatColor color) {
+    this.color = color;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setAllowFriendlyFire(boolean friendlyFire) {
+    this.allowFriendlyFire = friendlyFire;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setSeeFriendlyInvisible(boolean friendlyInvisible) {
+    this.canSeeFriendlyInvisible = friendlyInvisible;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setNameTagVisibility(VisibleType visibility) {
+    this.nameTagVisibility = visibility;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setDeathMessageVisibility(VisibleType visibility) {
+    this.deathMessageVisibility = visibility;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setCollisionType(CollisionType type) {
+    this.collisionType = type;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Collection<String> getMembers() {
+    return this.members;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ChatColor getTeamColor() {
+    return this.color;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean seeFriendlyInvisible() {
+    return this.canSeeFriendlyInvisible;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean allowFriendlyFire() {
+    return this.allowFriendlyFire;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public VisibleType getNameTagVisibility() {
+    return this.nameTagVisibility;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public VisibleType getDeathMessageVisibility() {
+    return this.deathMessageVisibility;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public CollisionType getCollisionType() {
+    return this.collisionType;
+  }
+
+}
