06a94fb8dbd065c3284a5c39453c98b14a3cb260
Leonard Seibold <leo@zrtx.de>
2020-10-09 23:09:35 +0200

    remove polymorphism 🤮

--- component/gui/webgui/ultralight/src/internal/java/net/labyfy/internal/webgui/ultralight/view/UltralightMainWebGuiView.java
+++ component/gui/webgui/ultralight/src/internal/java/net/labyfy/internal/webgui/ultralight/view/UltralightMainWebGuiView.java
@@ -4,60 +4,64 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.labyfy.component.gui.event.FramebufferSizeEvent;
 import net.labyfy.component.gui.event.GuiEvent;
+import net.labyfy.component.gui.event.GuiEventListener;
 import net.labyfy.component.gui.event.WindowFocusEvent;
 import net.labyfy.component.gui.windowing.MinecraftWindow;
-import net.labyfy.component.render.shader.ShaderProgram;
-import net.labyfy.component.render.shader.ShaderUniform;
-import net.labyfy.component.render.vbo.VertexArrayObject;
-import net.labyfy.component.render.vbo.VertexBufferObject;
-import net.labyfy.component.render.vbo.VertexIndexObject;
+import net.labyfy.component.gui.windowing.WindowRenderer;
 import net.labyfy.internal.webgui.ultralight.UltralightWebGuiController;
 import net.labyfy.webgui.MainWebGuiView;
 
 /** Extension of a windowed Ultralight view for the main window. */
 @Singleton
-public class UltralightMainWebGuiView extends UltralightWindowWebView implements MainWebGuiView {
+public class UltralightMainWebGuiView
+    implements MainWebGuiView, UltralightWebGuiView, WindowRenderer, GuiEventListener {
   private final UltralightWebGuiController controller;
   private final MinecraftWindow minecraftWindow;
+
+  private final UltralightWindowWebView windowWebView;
 
   private boolean allowFocus;
 
   @Inject
   protected UltralightMainWebGuiView(
-      ShaderProgram.Factory shaderFactory,
-      ShaderUniform.Factory uniformFactory,
-      VertexArrayObject.Factory vaoFactory,
-      VertexBufferObject.Factory vboFactory,
-      VertexIndexObject.Factory eboFactory,
+      UltralightWindowWebView.Factory windowWebViewFactory,
       UltralightWebGuiController controller,
       MinecraftWindow minecraftWindow) {
-    super(
-        shaderFactory,
-        uniformFactory,
-        vaoFactory,
-        vboFactory,
-        eboFactory,
-        controller,
-        minecraftWindow.getFramebufferWidth(),
-        minecraftWindow.getFramebufferHeight(),
-        true);
+    this.windowWebView =
+        windowWebViewFactory.create(
+            minecraftWindow.getFramebufferWidth(), minecraftWindow.getFramebufferHeight(), true);
     this.controller = controller;
     this.minecraftWindow = minecraftWindow;
     this.allowFocus = true;
   }
 
   @Override
+  public void initialize() {
+    this.windowWebView.initialize();
+  }
+
+  @Override
+  public boolean isIntrusive() {
+    return this.windowWebView.isIntrusive();
+  }
+
+  @Override
   public void render() {
     // Trigger rendering of all views now
-    controller.renderAll();
-    super.render();
+    this.controller.renderAll();
+    this.windowWebView.render();
+  }
+
+  @Override
+  public void cleanup() {
+    this.windowWebView.cleanup();
   }
 
   @Override
   public boolean handle(GuiEvent event) {
     if (event instanceof FramebufferSizeEvent) {
       // The framebuffer size event needs to be handled always (or the content will look weird)
-      super.handle(event);
+      this.windowWebView.handle(event);
       return false;
     }
 
@@ -65,7 +69,7 @@ public class UltralightMainWebGuiView extends UltralightWindowWebView implements
       return false; // Pass through events
     }
 
-    return super.handle(event);
+    return this.windowWebView.handle(event);
   }
 
   @Override
@@ -73,5 +77,35 @@ public class UltralightMainWebGuiView extends UltralightWindowWebView implements
     this.allowFocus = allow;
 
     handle(new WindowFocusEvent(allowFocus && minecraftWindow.isFocused()));
+  }
+
+  @Override
+  public void dataReadyOnSurface() {
+    this.windowWebView.dataReadyOnSurface();
+  }
+
+  @Override
+  public void dataReadyOnOpenGLTexture(int textureId) {
+    this.windowWebView.dataReadyOnOpenGLTexture(textureId);
+  }
+
+  @Override
+  public void close() {
+    this.windowWebView.close();
+  }
+
+  @Override
+  public void setTransparent(boolean transparent) {
+    this.windowWebView.setTransparent(transparent);
+  }
+
+  @Override
+  public void setURL(String url) {
+    this.windowWebView.setURL(url);
+  }
+
+  @Override
+  public void setScale(float scale) {
+    this.windowWebView.setScale(scale);
   }
 }
