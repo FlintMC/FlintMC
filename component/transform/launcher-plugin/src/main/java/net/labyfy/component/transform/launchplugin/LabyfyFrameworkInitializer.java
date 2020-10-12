b2a0a72fb48f3e814720d36aea2e0298874ec9d5
Jan <contact@jan-br.de>
2020-10-12 11:54:04 +0200

    add some docs

--- component/transform/launcher-plugin/src/main/java/net/labyfy/component/transform/launchplugin/LabyfyFrameworkInitializer.java
+++ component/transform/launcher-plugin/src/main/java/net/labyfy/component/transform/launchplugin/LabyfyFrameworkInitializer.java
@@ -30,14 +30,25 @@ public class LabyfyFrameworkInitializer {
   private LabyfyFrameworkInitializer() {
   }
 
+  /**
+   * Initializes all features of the labyfy framework.
+   *
+   * @param arguments minecraft launch arguments
+   */
   public void initialize(Map<String, String> arguments) {
     try {
+      //create guice constant module
       InjectionHolder.getInstance().addModules(new BindConstantModule(arguments));
+      //Apply module and instantiate service repository
       ServiceRepository serviceRepository = InjectionHolder.getInjectedInstance(ServiceRepository.class);
 
+      //Find all services on the classpath and register them to the service repository
       this.prepareServices(serviceRepository);
+      //Flush all registered services that are defined in the PRE_INIT state. This should only be done if it is really necessary.
       this.flushServices(Service.State.PRE_INIT, serviceRepository);
+      //Apply all Implementations and AssistedFactories
       InjectionHolder.getInjectedInstance(InjectionService.class).flush();
+      //Flush all other higher level framework features like Events, Transforms etc.
       this.flushServices(Service.State.POST_INIT, serviceRepository);
 
     } catch (NotFoundException e) {
@@ -58,15 +69,15 @@ public class LabyfyFrameworkInitializer {
     for (Pair<Service, CtClass> pair : services) {
       Service service = pair.getFirst();
       for (Class<? extends Annotation> annotationType : service.value()) {
-        for (Pair<AnnotationMeta<?>, Object> annotationMetaObjectPair : serviceRepository.getAnnotations().get(annotationType)) {
+        for (AnnotationMeta<?> annotationMeta : serviceRepository.getAnnotations().get(annotationType)) {
           try {
-            Pair<AnnotationMeta<?>, CtClass> serviceMetaPair = new Pair<>(annotationMetaObjectPair.getFirst(), pair.getSecond());
+            Pair<AnnotationMeta<?>, CtClass> serviceMetaPair = new Pair<>(annotationMeta, pair.getSecond());
             if (discoveredMeta.contains(serviceMetaPair)) continue;
             discoveredMeta.add(serviceMetaPair);
             if (!serviceHandlerInstances.containsKey(pair.getSecond())) {
               serviceHandlerInstances.put(pair.getSecond(), InjectionHolder.getInjectedInstance(CtResolver.get(pair.getSecond())));
             }
-            serviceHandlerInstances.get(pair.getSecond()).discover(annotationMetaObjectPair.getFirst());
+            serviceHandlerInstances.get(pair.getSecond()).discover(annotationMeta);
           } catch (Exception e) {
             e.printStackTrace();
           }
@@ -77,39 +88,36 @@ public class LabyfyFrameworkInitializer {
   }
 
   private void prepareServices(ServiceRepository serviceRepository) throws NotFoundException {
-    List<AnnotationMeta> annotations = getAnnotations();
+    //Discover all annotation meta data
+    List<AnnotationMeta> annotations = getAnnotationMeta();
+    //Iterate over all annotations
     for (AnnotationMeta annotationMeta : annotations) {
+      //check if metadata is a service
       if (annotationMeta.getAnnotation().annotationType().equals(Service.class)) {
+        //if yes go ahead and register it
         serviceRepository.registerService((Service) annotationMeta.getAnnotation(), ClassPool.getDefault().get(((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier())).getName()));
+        //if not check if it might be multiple services at once. the Javapoet framework sadly seems to not support Repeatable annotations yet. Maybe this will change sometime.
       } else if (annotationMeta.getAnnotation().annotationType().equals(Services.class)) {
+        //Iterate over all services and register them
         for (Service service : ((Services) annotationMeta.getAnnotation()).value()) {
           serviceRepository.registerService(service, ClassPool.getDefault().get(((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier())).getName()));
         }
       }
     }
 
+    //Iterate over all annotations again
     for (AnnotationMeta annotationMeta : annotations) {
-      if (annotationMeta.getAnnotation().annotationType().equals(Service.class) || annotationMeta.getAnnotation().annotationType().equals(Services.class))
-        continue;
-      if (annotationMeta.getIdentifier() instanceof AnnotationMeta.ClassIdentifier) {
-        serviceRepository.registerClassAnnotation(
-            annotationMeta,
-            ((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier())).getLocation()
-        );
-      } else if (annotationMeta.getIdentifier() instanceof AnnotationMeta.MethodIdentifier) {
-
-        AnnotationMeta.MethodIdentifier identifier = (AnnotationMeta.MethodIdentifier) annotationMeta.getIdentifier();
-
-        CtClass[] parameters = new CtClass[identifier.getParameters().length];
-        for (int i = 0; i < parameters.length; i++) {
-          parameters[i] = ClassPool.getDefault().get(identifier.getParameters()[i]);
-        }
-        serviceRepository.registerMethodAnnotation(annotationMeta, identifier.getLocation());
-      }
+      //register the annotation
+      serviceRepository.registerAnnotation(
+          annotationMeta
+      );
     }
   }
 
-  private List<AnnotationMeta> getAnnotations() {
+  /**
+   * @return all saved annotation meta that was written to the {@link DetectableAnnotationProvider} on compile time
+   */
+  private List<AnnotationMeta> getAnnotationMeta() {
     List<AnnotationMeta> annotationMetas = new ArrayList<>();
 
     Set<DetectableAnnotationProvider> discover = ExtendedServiceLoader.get(DetectableAnnotationProvider.class).discover(LaunchController.getInstance().getRootLoader());
