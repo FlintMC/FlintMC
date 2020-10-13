d9779b58dafa86965dbaca576232cbc9440bdf27
Leonard Seibold <leo@zrtx.de>
2020-10-13 02:07:07 +0200

    fix NullpointerException in ScreenNameMapper

--- component/gui/src/main/java/net/labyfy/component/gui/screen/ScreenNameMapper.java
+++ component/gui/src/main/java/net/labyfy/component/gui/screen/ScreenNameMapper.java
@@ -16,9 +16,13 @@ public interface ScreenNameMapper {
    * Converts the given object into a screen name.
    *
    * @param screen The screen object
-   * @return The converted screen name
+   * @return The converted screen name, or {@code null}, if screen is {@code null}
    */
   default ScreenName fromObject(Object screen) {
+    if(screen == null) {
+      return null;
+    }
+
     return fromClass(screen.getClass().getName());
   }
 }
