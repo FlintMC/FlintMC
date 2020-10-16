822edb3309f2a6c7e17269120e938bfb57deec68
R0bbyYT <r0bby@gmx.net>
2020-10-16 17:17:26 +0200

    Fixed AnimalEntity

--- component/minecraft/src/v1_15_2/java/net/labyfy/internal/component/entity/v1_15_2/passive/VersionedAnimalEntity.java
+++ component/minecraft/src/v1_15_2/java/net/labyfy/internal/component/entity/v1_15_2/passive/VersionedAnimalEntity.java
@@ -8,15 +8,19 @@ import net.labyfy.component.entity.passive.AnimalEntity;
 import net.labyfy.component.entity.type.EntityType;
 import net.labyfy.component.inject.implement.Implement;
 import net.labyfy.component.items.ItemStack;
+import net.labyfy.component.nbt.NBTCompound;
 import net.labyfy.component.nbt.mapper.NBTMapper;
 import net.labyfy.component.player.PlayerEntity;
+import net.labyfy.component.player.type.hand.Hand;
 import net.labyfy.component.world.World;
 import net.labyfy.internal.component.entity.v1_15_2.VersionedAgeableEntity;
+import net.minecraft.nbt.CompoundNBT;
 
 @Implement(value = AnimalEntity.class, version = "1.15.2")
 public class VersionedAnimalEntity extends VersionedAgeableEntity implements AnimalEntity {
 
   private final net.minecraft.entity.passive.AnimalEntity animalEntity;
+  private final NBTMapper nbtMapper;
 
   @AssistedInject
   public VersionedAnimalEntity(
@@ -33,7 +37,7 @@ public class VersionedAnimalEntity extends VersionedAgeableEntity implements Ani
       throw new IllegalArgumentException(entity.getClass().getName() + " is not an instance of " + net.minecraft.entity.passive.AnimalEntity.class.getName());
     }
 
-
+    this.nbtMapper = nbtMapper;
     this.animalEntity = (net.minecraft.entity.passive.AnimalEntity) entity;
   }
 
@@ -97,5 +101,50 @@ public class VersionedAnimalEntity extends VersionedAgeableEntity implements Ani
     return this.animalEntity.canMateWith(
             (net.minecraft.entity.passive.AnimalEntity) this.getEntityBaseMapper().getEntityMapper().toMinecraftAnimalEntity(entity)
     );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int getTalkInterval() {
+    return this.animalEntity.getTalkInterval();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean canDespawn(double distanceToClosestPlayer) {
+    return this.animalEntity.canDespawn(distanceToClosestPlayer);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean processInteract(PlayerEntity entity, Hand hand) {
+    return this.animalEntity.processInteract(
+            (net.minecraft.entity.player.PlayerEntity) this.getEntityBaseMapper().getEntityMapper().toMinecraftPlayerEntity(entity),
+            (net.minecraft.util.Hand) this.getEntityBaseMapper().getHandMapper().toMinecraftHand(hand)
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void readAdditional(NBTCompound compound) {
+    super.readAdditional(compound);
+    this.animalEntity.readAdditional((CompoundNBT) this.nbtMapper.toMinecraftNBT(compound));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void writeAdditional(NBTCompound compound) {
+    super.writeAdditional(compound);
+    this.animalEntity.writeAdditional((CompoundNBT) this.nbtMapper.toMinecraftNBT(compound));
   }
 }
