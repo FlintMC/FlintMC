dc374ca87bfaabf39272ff8c6033b2fc15a2c66e
Jan <contact@jan-br.de>
2020-10-23 12:28:57 +0200

    Some docs

--- component/stereotype/src/main/java/net/labyfy/component/stereotype/service/ServiceRepository.java
+++ component/stereotype/src/main/java/net/labyfy/component/stereotype/service/ServiceRepository.java
@@ -5,15 +5,15 @@ import com.google.common.collect.Multimap;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import javassist.CtClass;
-import net.labyfy.component.commons.util.Pair;
 import net.labyfy.component.processing.autoload.AnnotationMeta;
+import net.labyfy.component.stereotype.ServiceHandlerMeta;
 
 import java.lang.annotation.Annotation;
 
 @Singleton
 public class ServiceRepository {
 
-  private final Multimap<Class<? extends Annotation>, Pair<Service, CtClass>> serviceHandlers;
+  private final Multimap<Class<? extends Annotation>, ServiceHandlerMeta> serviceHandlers;
   private final Multimap<Class<? extends Annotation>, AnnotationMeta<?>> annotations;
 
   @Inject
@@ -22,25 +22,68 @@ public class ServiceRepository {
     this.annotations = HashMultimap.create();
   }
 
-  public void registerService(Service service, CtClass ctClass) {
-    for (Class<? extends Annotation> annotation : service.value()) {
-      this.registerService(annotation, service, ctClass);
+  /**
+   * Registers a service handler that can pickup {@link
+   * net.labyfy.component.processing.autoload.DetectableAnnotation}s.
+   *
+   * @param annotationTypes     the annotation types the service should handle
+   * @param priority            the service priority. Lower priority is called first
+   * @param state               the initialization state of the service. see also {@link
+   *                            net.labyfy.component.stereotype.service.Service.State} for usage
+   * @param serviceHandlerClass the service handler class to handle {@link
+   *                            net.labyfy.component.processing.autoload.DetectableAnnotation}s
+   */
+  public void registerService(
+      Class<? extends Annotation>[] annotationTypes,
+      int priority,
+      Service.State state,
+      CtClass serviceHandlerClass) {
+    for (Class<? extends Annotation> annotationType : annotationTypes) {
+      this.registerService(annotationType, priority, state, serviceHandlerClass);
     }
   }
 
-  public void registerService(Class<? extends Annotation> annotation, Service service, CtClass ctClass) {
-    this.serviceHandlers.put(annotation, new Pair<>(service, ctClass));
+  /**
+   * Registers a service handler that can pickup {@link
+   * net.labyfy.component.processing.autoload.DetectableAnnotation}s.
+   *
+   * @param annotationType      the annotation type the service should handle
+   * @param priority            the service priority. Lower priority is called first
+   * @param state               the initialization state of the service. see also {@link
+   *                            net.labyfy.component.stereotype.service.Service.State} for usage
+   * @param serviceHandlerClass the service handler class to handle {@link
+   *                            net.labyfy.component.processing.autoload.DetectableAnnotation}s
+   */
+  public void registerService(
+      Class<? extends Annotation> annotationType,
+      int priority,
+      Service.State state,
+      CtClass serviceHandlerClass) {
+    this.serviceHandlers.put(
+        annotationType, ServiceHandlerMeta.create(annotationType, priority, state, serviceHandlerClass));
   }
 
+  /**
+   * Registers a {@link net.labyfy.component.processing.autoload.DetectableAnnotation} and provides
+   * ot to the service handlers.
+   *
+   * @param annotationMeta the annotation meta of the {@link net.labyfy.component.processing.autoload.DetectableAnnotation} to register.
+   */
   public void registerAnnotation(AnnotationMeta<?> annotationMeta) {
     this.annotations.put(annotationMeta.getAnnotation().annotationType(), annotationMeta);
   }
 
+  /**
+   * @return all registered annotations
+   */
   public Multimap<Class<? extends Annotation>, AnnotationMeta<?>> getAnnotations() {
     return HashMultimap.create(annotations);
   }
 
-  public Multimap<Class<? extends Annotation>, Pair<Service, CtClass>> getServiceHandlers() {
+  /**
+   * @return all registered service handlers
+   */
+  public Multimap<Class<? extends Annotation>, ServiceHandlerMeta> getServiceHandlers() {
     return HashMultimap.create(serviceHandlers);
   }
 }
--- component/transform/launcher-plugin/src/main/java/net/labyfy/component/transform/launchplugin/LabyfyFrameworkInitializer.java
+++ component/transform/launcher-plugin/src/main/java/net/labyfy/component/transform/launchplugin/LabyfyFrameworkInitializer.java
@@ -12,6 +12,7 @@ import net.labyfy.component.launcher.LaunchController;
 import net.labyfy.component.processing.autoload.AnnotationMeta;
 import net.labyfy.component.processing.autoload.DetectableAnnotationProvider;
 import net.labyfy.component.service.ExtendedServiceLoader;
+import net.labyfy.component.stereotype.ServiceHandlerMeta;
 import net.labyfy.component.stereotype.service.*;
 import net.labyfy.component.transform.launchplugin.inject.module.BindConstantModule;
 
@@ -21,10 +22,8 @@ import java.util.*;
 @Singleton
 public class LabyfyFrameworkInitializer {
 
-
   private final Map<CtClass, ServiceHandler> serviceHandlerInstances = new HashMap<>();
   private final Collection<Pair<AnnotationMeta<?>, CtClass>> discoveredMeta = new HashSet<>();
-
 
   @Inject
   private LabyfyFrameworkInitializer() {
@@ -37,53 +36,61 @@ public class LabyfyFrameworkInitializer {
    */
   public void initialize(Map<String, String> arguments) {
     try {
-      LaunchController.getInstance().getRootLoader().excludeFromModification("net.labyfy.internal.component.transform.");
+      LaunchController.getInstance()
+          .getRootLoader()
+          .excludeFromModification("net.labyfy.internal.component.transform.");
 
-      //create guice constant module
+      // create guice constant module
       InjectionHolder.getInstance().addModules(new BindConstantModule(arguments));
-      //Apply module and instantiate service repository
-      ServiceRepository serviceRepository = InjectionHolder.getInjectedInstance(ServiceRepository.class);
+      // Apply module and instantiate service repository
+      ServiceRepository serviceRepository =
+          InjectionHolder.getInjectedInstance(ServiceRepository.class);
 
-      //Find all services on the classpath and register them to the service repository
+      // Find all services on the classpath and register them to the service repository
       this.prepareServices(serviceRepository);
-      //Flush all registered services that are defined in the PRE_INIT state. This should only be done if it is really necessary.
+      // Flush all registered services that are defined in the PRE_INIT state. This should only be
+      // done if it is really necessary.
       this.flushServices(Service.State.PRE_INIT, serviceRepository);
-      //Apply all Implementations and AssistedFactories
+      // Apply all Implementations and AssistedFactories
       InjectionHolder.getInjectedInstance(InjectionService.class).flush();
-      //Flush all other higher level framework features like Events, Transforms etc.
+      // Flush all other higher level framework features like Events, Transforms etc.
       this.flushServices(Service.State.POST_INIT, serviceRepository);
 
     } catch (NotFoundException e) {
       e.printStackTrace();
     }
-
   }
 
   private void flushServices(Service.State state, ServiceRepository serviceRepository) {
-    List<Pair<Service, CtClass>> services = new ArrayList<>();
-    for (Pair<Service, CtClass> value : serviceRepository.getServiceHandlers().values()) {
-      if (!value.getFirst().state().equals(state)) continue;
+    List<ServiceHandlerMeta> services = new ArrayList<>();
+    for (ServiceHandlerMeta value : serviceRepository.getServiceHandlers().values()) {
+      if (!value.getState().equals(state)) continue;
       services.add(value);
     }
 
-    services.sort(Comparator.comparingInt(pair -> pair.getFirst().priority()));
+    services.sort(Comparator.comparingInt(ServiceHandlerMeta::getPriority));
 
-    for (Pair<Service, CtClass> pair : services) {
-      Service service = pair.getFirst();
-      for (Class<? extends Annotation> annotationType : service.value()) {
-        for (AnnotationMeta<?> annotationMeta : serviceRepository.getAnnotations().get(annotationType)) {
+    for (ServiceHandlerMeta serviceHandlerMeta : services) {
+      CtClass serviceHandlerClass = serviceHandlerMeta.getServiceHandlerClass();
+      for (Class<? extends Annotation> annotationType : serviceHandlerMeta.getAnnotationTypes()) {
+        for (AnnotationMeta<?> annotationMeta :
+            serviceRepository.getAnnotations().get(annotationType)) {
           try {
-            Pair<AnnotationMeta<?>, CtClass> serviceMetaPair = new Pair<>(annotationMeta, pair.getSecond());
+            Pair<AnnotationMeta<?>, CtClass> serviceMetaPair =
+                new Pair<>(annotationMeta, serviceHandlerClass);
             if (discoveredMeta.contains(serviceMetaPair)) continue;
             discoveredMeta.add(serviceMetaPair);
-            if (!serviceHandlerInstances.containsKey(pair.getSecond())) {
-              serviceHandlerInstances.put(pair.getSecond(), InjectionHolder.getInjectedInstance(CtResolver.get(pair.getSecond())));
+            if (!serviceHandlerInstances.containsKey(serviceHandlerClass)) {
+              serviceHandlerInstances.put(
+                  serviceHandlerClass,
+                  InjectionHolder.getInjectedInstance(
+                      CtResolver.get(serviceHandlerClass)));
             }
-            if(!pair.getSecond().isFrozen()){
-              pair.getSecond().freeze();
+            if (!serviceHandlerClass.isFrozen()) {
+              serviceHandlerClass.freeze();
             }
-            serviceHandlerInstances.get(pair.getSecond()).discover(annotationMeta);
-            pair.getSecond().defrost();
+            serviceHandlerInstances.get(serviceHandlerClass).discover(annotationMeta);
+            serviceHandlerClass.defrost();
 
           } catch (Exception e) {
             e.printStackTrace();
@@ -91,45 +98,60 @@ public class LabyfyFrameworkInitializer {
         }
       }
     }
-
   }
 
   private void prepareServices(ServiceRepository serviceRepository) throws NotFoundException {
-    //Discover all annotation meta data
+    // Discover all annotation meta data
     List<AnnotationMeta> annotations = getAnnotationMeta();
-    //Iterate over all annotations
+    // Iterate over all annotations
     for (AnnotationMeta annotationMeta : annotations) {
-      //check if metadata is a service
+      // check if metadata is a service
       if (annotationMeta.getAnnotation().annotationType().equals(Service.class)) {
-        //if yes go ahead and register it
-        serviceRepository.registerService((Service) annotationMeta.getAnnotation(), ClassPool.getDefault().get(((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier())).getName()));
-        //if not check if it might be multiple services at once. the Javapoet framework sadly seems to not support Repeatable annotations yet. Maybe this will change sometime.
+        // if yes go ahead and register it
+        Service annotation = (Service) annotationMeta.getAnnotation();
+        serviceRepository.registerService(
+            annotation.value(),
+            annotation.priority(),
+            annotation.state(),
+            ClassPool.getDefault()
+                .get(
+                    ((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier())).getName()));
+        // if not check if it might be multiple services at once. the Javapoet framework sadly seems
+        // to not support Repeatable annotations yet. Maybe this will change sometime.
       } else if (annotationMeta.getAnnotation().annotationType().equals(Services.class)) {
-        //Iterate over all services and register them
+        // Iterate over all services and register them
         for (Service service : ((Services) annotationMeta.getAnnotation()).value()) {
-          serviceRepository.registerService(service, ClassPool.getDefault().get(((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier())).getName()));
+          serviceRepository.registerService(
+              service.value(),
+              service.priority(),
+              service.state(),
+              ClassPool.getDefault()
+                  .get(
+                      ((AnnotationMeta.ClassIdentifier) (annotationMeta.getIdentifier()))
+                          .getName()));
         }
       }
     }
 
-    //Iterate over all annotations again
+    // Iterate over all annotations again
     for (AnnotationMeta annotationMeta : annotations) {
-      //register the annotation
-      serviceRepository.registerAnnotation(
-          annotationMeta
-      );
+      // register the annotation
+      serviceRepository.registerAnnotation(annotationMeta);
     }
   }
 
   /**
-   * @return all saved annotation meta that was written to the {@link DetectableAnnotationProvider} on compile time
+   * @return all saved annotation meta that was written to the {@link DetectableAnnotationProvider}
+   * on compile time
    */
   private List<AnnotationMeta> getAnnotationMeta() {
     List<AnnotationMeta> annotationMetas = new ArrayList<>();
 
-    Set<DetectableAnnotationProvider> discover = ExtendedServiceLoader.get(DetectableAnnotationProvider.class).discover(LaunchController.getInstance().getRootLoader());
-    discover.forEach(detectableAnnotationProvider -> detectableAnnotationProvider.register(annotationMetas));
+    Set<DetectableAnnotationProvider> discover =
+        ExtendedServiceLoader.get(DetectableAnnotationProvider.class)
+            .discover(LaunchController.getInstance().getRootLoader());
+    discover.forEach(
+        detectableAnnotationProvider -> detectableAnnotationProvider.register(annotationMetas));
     return annotationMetas;
   }
-
 }
