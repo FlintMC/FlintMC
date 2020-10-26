ca02899579bd09a8b1904d5d0245bed3f6764e25
Jan <contact@jan-br.de>
2020-10-26 20:37:45 +0100

    add docs

--- component/annotation-processing/autoload/src/main/java/net/labyfy/component/processing/autoload/RepeatingDetectableAnnotation.java
+++ component/annotation-processing/autoload/src/main/java/net/labyfy/component/processing/autoload/RepeatingDetectableAnnotation.java
@@ -2,10 +2,14 @@ package net.labyfy.component.processing.autoload;
 
 import java.lang.annotation.*;
 
+/**
+ * Marks a {@link DetectableAnnotation} that is repeatable. The repeated annotation must be
+ * annotated with @{@link Repeatable}. The repeating annotation must be annotated with {@link
+ * RepeatingDetectableAnnotation}
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface RepeatingDetectableAnnotation {
 
   Class<? extends Annotation> value() default Annotation.class;
-
 }
