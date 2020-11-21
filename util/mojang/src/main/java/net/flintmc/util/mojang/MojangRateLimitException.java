56ddd60b626b43c9ecc70661435f216435ea2436
Robin <derrop@outlook.de>
2020-11-21 16:22:42 +0100

    Add javadocs to the MojangRateLimitException

--- util/mojang/src/main/java/net/flintmc/util/mojang/MojangRateLimitException.java
+++ util/mojang/src/main/java/net/flintmc/util/mojang/MojangRateLimitException.java
@@ -1,5 +1,12 @@
 package net.flintmc.util.mojang;
 
+import net.flintmc.util.mojang.history.NameHistoryResolver;
+import net.flintmc.util.mojang.profile.GameProfileResolver;
+
+/**
+ * This exception is being fired by the {@link GameProfileResolver} and {@link NameHistoryResolver}
+ * when Mojang has rate limited our IP address.
+ */
 public class MojangRateLimitException extends RuntimeException {
 
   public static MojangRateLimitException INSTANCE = new MojangRateLimitException();
