5b96f06e1ee4ab2e0610b6c7676758103b5ef0ef
Jan <contact@jan-br.de>
2020-12-07 16:22:12 +0100

    Add doc

--- util/property/src/main/java/net/flintmc/util/property/PropertyContextAware.java
+++ util/property/src/main/java/net/flintmc/util/property/PropertyContextAware.java
@@ -1,6 +1,15 @@
 package net.flintmc.util.property;
 
+/**
+ * Represents an owner of a {@link PropertyContext}.
+ *
+ * @param <T_PropertyContextAware> self reference. Just used for generic locking
+ * @see PropertyContext
+ */
 public interface PropertyContextAware<
     T_PropertyContextAware extends PropertyContextAware<T_PropertyContextAware>> {
+  /**
+   * @return the instance of the associated {@link PropertyContext}
+   */
   PropertyContext<T_PropertyContextAware> getPropertyContext();
 }
