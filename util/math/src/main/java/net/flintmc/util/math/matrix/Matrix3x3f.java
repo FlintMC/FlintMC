b21a0995c8f5acc892c7d238ee163d9159a881d3
Jan <contact@jan-br.de>
2020-12-08 10:44:05 +0100

    Add docs

--- util/math/src/main/java/net/flintmc/util/math/matrix/Matrix3x3f.java
+++ util/math/src/main/java/net/flintmc/util/math/matrix/Matrix3x3f.java
@@ -2,10 +2,19 @@ package net.flintmc.util.math.matrix;
 
 import net.flintmc.framework.inject.assisted.AssistedFactory;
 
+/**
+ * Float implementation of {@link Matrix3x3}
+ */
 public interface Matrix3x3f extends Matrix3x3<Float, Matrix3x3f> {
 
+  /**
+   * A factory class for {@link Matrix3x3f}.
+   */
   @AssistedFactory(Matrix3x3f.class)
   interface Factory {
+    /**
+     * @return an identity matrix3x3
+     */
     Matrix3x3f create();
   }
 
