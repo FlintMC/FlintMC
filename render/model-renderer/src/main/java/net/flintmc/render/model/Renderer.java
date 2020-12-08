5079d5276ee68b00c244e678d9df4c3230866a27
Jan <contact@jan-br.de>
2020-12-08 11:42:19 +0100

    Add docs

--- render/model-renderer/src/main/java/net/flintmc/render/model/Renderer.java
+++ render/model-renderer/src/main/java/net/flintmc/render/model/Renderer.java
@@ -13,7 +13,24 @@ public interface Renderer<
     T_RenderContext extends RenderContext<?, ?, T_Renderable, T_RenderMeta, ?>,
     T_RenderMeta> {
 
+  /**
+   * Render the given {@link Renderable}. The context in which this will be called depends on the
+   * implementation of the underlying layers. All Flint implementations will always provide a valid
+   * OpenGL State for the target {@link Renderable}.
+   *
+   * @param renderable the {@link Renderable} that should be rendered
+   * @param renderMeta the metadata required to render the renderable. If not needed, use {@link
+   *                   Void}
+   */
   void render(T_Renderable renderable, T_RenderMeta renderMeta);
 
+  /**
+   * Defines if this {@link Renderer} ends the current render round.
+   *
+   * @param renderable the {@link Renderable} that should be rendered
+   * @param renderMeta the metadata required to render the renderable. If not needed, use {@link
+   *                   Void}
+   * @return this
+   */
   boolean shouldExecuteNextStage(T_Renderable renderable, T_RenderMeta renderMeta);
 }
