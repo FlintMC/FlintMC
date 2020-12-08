dc09afc8e34c4544f56351beb41e3e892578198f
Julian <42209193+juliarn@users.noreply.github.com>
2020-12-08 15:09:08 +0100

    Adjust documentation of MinecraftInitializeEvent

--- mcapi/src/main/java/net/flintmc/mcapi/event/MinecraftInitializeEvent.java
+++ mcapi/src/main/java/net/flintmc/mcapi/event/MinecraftInitializeEvent.java
@@ -4,8 +4,7 @@ import net.flintmc.framework.eventbus.event.Event;
 import net.flintmc.framework.eventbus.event.subscribe.Subscribe;
 
 /**
- * This event will be fired when Minecraft is initialized, which is done in the Minecraft class
- * constructor. It will be fired in both {@link Subscribe.Phase#PRE} and {@link
- * Subscribe.Phase#POST} phases, at first and at last in the constructor.
+ * This event will be fired when Minecraft is initialized. It will be fired in both {@link
+ * Subscribe.Phase#PRE} and {@link Subscribe.Phase#POST} phases.
  */
 public interface MinecraftInitializeEvent extends Event {}
