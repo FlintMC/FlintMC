435ebab12485197c1d1c8460da50dc6841b756ed
R0bbyYT <r0bby@gmx.net>
2020-12-23 13:57:22 +0100

    Improved the payload injection.

--- mcapi/src/v1_15_2/java/net/flintmc/mcapi/v1_15_2/server/payload/CustomPayloadInterceptor.java
+++ mcapi/src/v1_15_2/java/net/flintmc/mcapi/v1_15_2/server/payload/CustomPayloadInterceptor.java
@@ -2,6 +2,9 @@ package net.flintmc.mcapi.v1_15_2.server.payload;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -14,6 +17,7 @@ import javassist.bytecode.CodeIterator;
 import javassist.bytecode.MethodInfo;
 import javassist.bytecode.Opcode;
 import net.flintmc.framework.inject.InjectedFieldBuilder;
+import net.flintmc.mcapi.server.ConnectedServer;
 import net.flintmc.mcapi.server.payload.PayloadChannelService;
 import net.flintmc.transform.javassist.ClassTransform;
 import net.flintmc.transform.javassist.ClassTransformContext;
@@ -33,7 +37,9 @@ public class CustomPayloadInterceptor {
 
   @Inject
   private CustomPayloadInterceptor(
-          ClassPool pool, ClassMappingProvider classMappingProvider, InjectedFieldBuilder.Factory fieldBuilderFactory) {
+      ClassPool pool,
+      ClassMappingProvider classMappingProvider,
+      InjectedFieldBuilder.Factory fieldBuilderFactory) {
     this.pool = pool;
     this.customPayloadPacketMapping =
         classMappingProvider.get("net.minecraft.network.play.server.SCustomPayloadPlayPacket");
@@ -58,6 +64,20 @@ public class CustomPayloadInterceptor {
     CodeIterator codeIterator = codeAttribute.iterator();
 
     int count = 0;
+
+    CtField connectedInjectedService =
+        this.fieldBuilderFactory
+            .create()
+            .target(method.getDeclaringClass())
+            .inject(ConnectedServer.class)
+            .generate();
+
+    method.insertBefore(
+        String.format(
+            "{%s.retrieveCustomPayload($1.%s().toString(), $1.%s());}",
+            connectedInjectedService.getName(),
+            this.customPayloadPacketMapping.getMethod("getChannelName").getName(),
+            this.customPayloadPacketMapping.getMethod("getBufferData").getName()));
 
     while (codeIterator.hasNext()) {
       int index = codeIterator.next();
