838fe92aaa33689cc96dd02f75e5e0bc0a56690c
Jan Brachthäuser <contact@jan-br.de>
2021-04-23 20:41:45 +0200

    mapping hotfix

--- minecraft/minecraft-1-15-2/build.gradle.kts
+++ minecraft/minecraft-1-15-2/build.gradle.kts
@@ -31,26 +31,6 @@ flint {
             from("build/generated/flint/version.json")
             to("versions/flint-1.15.2/flint-1.15.2.json")
         }
-
-        create("joined.tsrg") {
-            from("jar:https://files.minecraftforge.net/maven/de/oceanlabs/mcp/mcp_config/1.15.2-20200515.085601/mcp_config-1.15.2-20200515.085601.zip!/config/joined.tsrg")
-            to("flint/assets/1.15.2/joined.tsrg")
-        }
-
-        create("fields.csv") {
-            from("jar:https://files.minecraftforge.net/maven/de/oceanlabs/mcp/mcp_snapshot/20200610-1.15.1/mcp_snapshot-20200610-1.15.1.zip!/fields.csv")
-            to("flint/assets/1.15.2/fields.csv")
-        }
-
-        create("params.csv") {
-            from("jar:https://files.minecraftforge.net/maven/de/oceanlabs/mcp/mcp_snapshot/20200610-1.15.1/mcp_snapshot-20200610-1.15.1.zip!/params.csv")
-            to("flint/assets/1.15.2/params.csv")
-        }
-
-        create("methods.csv") {
-            from("jar:https://files.minecraftforge.net/maven/de/oceanlabs/mcp/mcp_snapshot/20200610-1.15.1/mcp_snapshot-20200610-1.15.1.zip!/methods.csv")
-            to("flint/assets/1.15.2/methods.csv")
-        }
     }
 }
 
