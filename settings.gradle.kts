bca3d5e1b2e2c9eb5bd95d6db8e6e3182b0b6d9b
jan <contact@jan-br.de>
2021-06-09 14:02:05 +0200

    bump flint gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,12 +26,12 @@ fun defineModule(path: String) {
 
 pluginManagement {
     plugins {
-        id("net.flintmc.flint-gradle") version "2.11.0"
+        id("net.flintmc.flint-gradle") version "2.11.1"
     }
 
     buildscript {
         dependencies {
-            classpath("net.flintmc", "flint-gradle", "2.11.0")
+            classpath("net.flintmc", "flint-gradle", "2.11.1")
         }
         repositories {
             mavenLocal()
